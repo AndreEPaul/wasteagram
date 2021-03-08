097a8eee075db381c24bdf4a9d7d56566421a10a
Andre Paul <andrepaul8128@gmail.com>
2021-03-08 14:46:57 -0800

    change color

--- lib/app.dart
+++ lib/app.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Wasteagram',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
       home: MyHomePage(title: 'Wasteagram'),
     );
